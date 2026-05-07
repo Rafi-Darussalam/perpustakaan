3150ded9738bec73507d45d784c7fb4960908ef0
ラフィ <70844466+Rafi-Darussalam@users.noreply.github.com>
2026-05-07 12:16:17 +0700

    menghapus import yang tidak terpakai

--- frontend/src/renderer/src/pages/Manajemen-pinjaman/Table.tsx
+++ frontend/src/renderer/src/pages/Manajemen-pinjaman/Table.tsx
@@ -1,6 +1,6 @@
 import { DataTable } from '@renderer/components/ui/data-table'
 import { ColumnDef } from '@tanstack/react-table'
-import { ArrowUpDown, MoreHorizontal, Loader2 } from 'lucide-react'
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
